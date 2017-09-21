2b1acedf00e1468a31ec43d5aedb85e336c0eaad
akgjohansson <johansson.mcquack@gmail.com>
2017-09-21 11:54:43 +0200

    Bygger databasåterställare

---
+++ Battleships.net/DataBase/HandleDataBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using NHibernate.Cfg;
+using System.Linq;
+using System.Web;
+
+namespace Battleships.net.DataBase
+{
+    /// <summary>
+    /// Bygger databasen utifrån mappningen
+    /// </summary>
+    public class HandleDataBase
+    {
+        public void BuildDataBase()
+        {
+            Configuration config = new Configuration();
+
+        }
+    }
+}
