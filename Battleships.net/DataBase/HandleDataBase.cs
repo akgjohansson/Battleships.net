a95a7784d7f4d1cc28f6c2050b905cd1cab82671
akgjohansson <johansson.mcquack@gmail.com>
2017-09-21 12:47:42 +0200

    Lade till en databashanterare

--- Battleships.net/DataBase/HandleDataBase.cs
+++ Battleships.net/DataBase/HandleDataBase.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using NHibernate.Cfg;
 using System.Linq;
 using System.Web;
+using NHibernate.Tool.hbm2ddl;
+using NHibernate.Cfg.MappingSchema;
+using NHibernate.Mapping.ByCode;
+using System.Reflection;
+using NHibernate.Dialect;
+
 
 namespace Battleships.net.DataBase
 {
@@ -14,6 +20,21 @@ namespace Battleships.net.DataBase
         public void BuildDataBase()
         {
             Configuration config = new Configuration();
+            Configuration cfg = new Configuration()
+                           .DataBaseIntegration(db =>
+                           {
+                               db.ConnectionString = @"Server = (localdb)\mssqllocaldb; Database = Battleships; Trusted_Connection = True;";
+                               db.Dialect<MsSql2008Dialect>();
+                           });
+
+            var mapper = new ModelMapper();
+            Type[] myTypes = Assembly.GetExecutingAssembly().GetExportedTypes();
+            mapper.AddMappings(myTypes);
+
+            HbmMapping mapping = new AutoMapper().Map();
+            cfg.AddMapping(mapping);
+            var schema = new SchemaExport(cfg);
+            schema.SetOutputFile("outputFile.dll");
 
         }
     }
