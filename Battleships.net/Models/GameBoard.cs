82877d3a4126c3d09de6f957053c7b447deb2ea1
akgjohansson <johansson.mcquack@gmail.com>
2017-09-22 10:38:55 +0200

    Lade till DropBomb

--- Battleships.net/Models/GameBoard.cs
+++ Battleships.net/Models/GameBoard.cs
@@ -9,6 +9,21 @@ namespace Battleships.net.Models
     public class GameBoard
     {
         public Game Game { get; set; }
-        public List<Grid> Grid { get; set; }
+        public Dictionary<string,Grid> Grid { get; set; }
+
+        public bool DropBomb(string coordinate)
+        {
+            if (Grid[coordinate.ToUpperInvariant].IsHit)
+            {
+                return false;
+            }
+            else
+            {
+                DropThisBomb(coordinate.ToUpper());
+                return true;
+            }
+        }
+
+
     }
 }
